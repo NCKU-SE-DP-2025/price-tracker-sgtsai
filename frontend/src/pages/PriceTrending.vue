59f5588caa4c9abe3e664af552d9d0eff51e5652
tomit <stonetsai96@gmail.com>
2025-09-30 09:24:28 +0800

    Refactor PriceTrending.vue to Composition API

--- frontend/src/pages/PriceTrending.vue
+++ frontend/src/pages/PriceTrending.vue
@@ -1,118 +1,120 @@
 <template>
-    <div class="wrapper">
-        <h1>物價趨勢</h1>
-        <div class="content">
-            <div class="selects">
-
-                <select v-model="selectedCategory">
-                    <option disabled value="">請選擇商品類別</option>
-                    <option v-for="category in categoryKeys" :key="category" :value="category">{{
-                        categoryName(category)}}</option>
-                </select>
-                <select v-model="selectedProduct">
-                    <option disabled value="">請選擇商品</option>
-                    <option v-for="product in products" :key="product.產品名稱" :value="product">{{ product.產品名稱 }}</option>
-                </select>
-            </div>
-            <div v-if="selectedProduct" class="visualize">
-                <TrendingChart v-if="selectedProduct" :data="selectedProduct"></TrendingChart>
-                <TrendingTable v-if="selectedProduct" :data="selectedProduct"></TrendingTable>
-            </div>
-        </div>
+  <div class="wrapper">
+    <h1>物價趨勢</h1>
+    <div class="content">
+      <div class="selects">
+        <select v-model="selectedCategory">
+          <option disabled value="">請選擇商品類別</option>
+          <option
+            v-for="category in categoryKeys"
+            :key="category"
+            :value="category"
+          >
+            {{ categoryName(category) }}
+          </option>
+        </select>
+        <select v-model="selectedProduct">
+          <option disabled value="">請選擇商品</option>
+          <option
+            v-for="product in products"
+            :key="product.產品名稱"
+            :value="product"
+          >
+            {{ product.產品名稱 }}
+          </option>
+        </select>
+      </div>
+      <div v-if="selectedProduct" class="visualize">
+        <TrendingChart :data="selectedProduct" />
+        <TrendingTable :data="selectedProduct" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import { ref, computed, watch, onMounted } from 'vue';
 import { usePricesStore } from '@/stores/prices';
 import Categories from '@/constants/categories';
 import TrendingTable from '@/components/TrendingTable.vue';
 import TrendingChart from '@/components/TrendingChart.vue';
 
 export default {
-    components: {
-        TrendingTable,
-        TrendingChart
-    },
-    data() {
-        return {
-            selectedCategory: '',
-            selectedProduct: '',
-            productList: [],
-        };
-    },
-    computed: {
-        store() {
-            return usePricesStore();
-        },
-        categoryKeys() {
-            return Object.keys(Categories);
-        },
-        products() {
-            return this.selectedCategory ? this.store.getPricesByCategory(this.selectedCategory) : [];
-        },
-    },
-    methods: {
-        categoryName(category) {
-            return Categories[category];
-        }
-    },
-    watch: {
-        selectedCategory() {
-            this.selectedProduct = '';
-            const store = usePricesStore();
-            this.productList = store.getProductList(this.selectedCategory);
-            this.productData = null;
-        },
-        selectedProduct() {
-            console.log(this.selectedProduct);
-        }
-    },
-    created() {
-        const store = usePricesStore();
-        store.fetchPrices();
-    }
+  name: 'PriceTrending',
+  components: {
+    TrendingTable,
+    TrendingChart
+  },
+  setup() {
+    const selectedCategory = ref('');
+    const selectedProduct = ref('');
+    const store = usePricesStore();
+
+    const categoryKeys = computed(() => Object.keys(Categories));
+    const products = computed(() =>
+      selectedCategory.value
+        ? store.getPricesByCategory(selectedCategory.value)
+        : []
+    );
+
+    const categoryName = category => Categories[category];
+
+    watch(selectedCategory, () => {
+      selectedProduct.value = '';
+    });
+
+    onMounted(() => {
+      store.fetchPrices();
+    });
+
+    return {
+      selectedCategory,
+      selectedProduct,
+      categoryKeys,
+      products,
+      categoryName
+    };
+  }
 };
 </script>
 
-
 <style scoped>
 .wrapper {
-    padding: 3em 5em;
-    background: #f3f3f3;
-    min-height: calc(100vh - 4.5em);
-    height: calc(100% - 4.5em);
-    box-sizing: border-box;
-    width: 100%;
+  padding: 3em 5em;
+  background: #f3f3f3;
+  min-height: calc(100vh - 4.5em);
+  height: calc(100% - 4.5em);
+  box-sizing: border-box;
+  width: 100%;
 }
 
 .content {
-    margin-top: 2em;
-    background-color: #fff;
-    border-radius: 1em;
-    padding: 2em;
-    width: 100%;
+  margin-top: 2em;
+  background-color: #fff;
+  border-radius: 1em;
+  padding: 2em;
+  width: 100%;
 }
-
 
 .selects {
-    display: flex;
-    justify-content: flex-start;
+  display: flex;
+  justify-content: flex-start;
 }
 
-.selects>select {
-    padding: .5em;
-    font-size: 1.1em;
-    margin-right: 1em;
-    border-radius: .5em;
-    border: 1px solid #ccc;
-    outline: none;
-    cursor: pointer;
-    appearance: auto !important;
+.selects > select {
+  padding: 0.5em;
+  font-size: 1.1em;
+  margin-right: 1em;
+  border-radius: 0.5em;
+  border: 1px solid #ccc;
+  outline: none;
+  cursor: pointer;
+  appearance: auto !important;
 }
 
 .visualize > * {
-    flex: 1 1 50%;
-    box-sizing: border-box;
-    padding: 1em;
+  flex: 1 1 50%;
+  box-sizing: border-box;
+  padding: 1em;
 }
 </style>
